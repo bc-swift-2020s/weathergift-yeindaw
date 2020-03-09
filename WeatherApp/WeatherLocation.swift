025750c39941ba7667d2c8c22f3c0bc45f5be024
Dawin Ye <yedawin@gmail.com>
2020-03-09 15:37:20 -0400

    7.2 done

---
+++ WeatherApp/WeatherLocation.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherLocation.swift
+//  WeatherApp
+//
+//  Created by Dawin Ye on 3/9/20.
+//  Copyright © 2020 Dawin Ye. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherLocation{
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
