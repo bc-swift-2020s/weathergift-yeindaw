6f5621917761042fd3ac0584287ea6c7c27f5380
Dawin Ye <yedawin@gmail.com>
2020-03-25 19:55:57 -0700

    7.9

--- WeatherApp/LocationDetailViewController.swift
+++ WeatherApp/LocationDetailViewController.swift
@@ -9,12 +9,13 @@
 import UIKit
 
 class LocationDetailViewController: UIViewController {
-
+    
     @IBOutlet weak var dateLabel: UILabel!
     @IBOutlet weak var placeLabel: UILabel!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var summaryLabel: UILabel!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var pageControl: UIPageControl!
     
     var weatherLocation: WeatherLocation!
     var locationIndex = 0
@@ -34,7 +35,8 @@ class LocationDetailViewController: UIViewController {
         placeLabel.text = weatherLocation.name
         temperatureLabel.text = "--°"
         summaryLabel.text = ""
-        
+        pageControl.numberOfPages = pageViewController.weatherLocations.count
+        pageControl.currentPage = locationIndex
         
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
@@ -50,4 +52,15 @@ class LocationDetailViewController: UIViewController {
         pageViewController.weatherLocations = source.weatherLocations
         pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: locationIndex)], direction: .forward, animated: false, completion: nil)
     }
+    
+    @IBAction func pageControlTapped(_ sender: UIPageControl) {
+        let pageViewController = UIApplication.shared.windows.first!.rootViewController as! PageViewController
+        var direction: UIPageViewController.NavigationDirection = .forward
+        if sender.currentPage < locationIndex {
+            direction = .reverse
+        }
+            pageViewController.setViewControllers([pageViewController.createLocationDetailViewController(forPage: sender.currentPage)], direction: direction, animated: true, completion: nil)
+    }
+    
+    
 }
